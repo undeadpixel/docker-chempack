a94d7b5773c2ba5e5af3233a7b023283a5da99ee
Josep Arús-Pous <josep.arus@dcb.unibe.ch>
2018-10-11 08:18:03 +0000

    updated versions; removed R

--- build_chempack.py
+++ build_chempack.py
@@ -5,13 +5,13 @@ import builder
 class ChemPackDockerImageBuilder(builder.DockerImageBuilder):
 
     def _get_version_name(self):
-         return "java{}-python{}-r{}-rdkit{}".format(self._parsed_args['java_version'], self._parsed_args['python_version'], self._parsed_args['r_version'], self._parsed_args['rdkit_release'])
+         return "java{}-python{}-rdkit{}".format(self._parsed_args['java_version'], self._parsed_args['python_version'], self._parsed_args['rdkit_release'])
 
     def _get_folder(self):
         return "base"
 
 
 if __name__ == '__main__':
-    image_builder = ChemPackDockerImageBuilder('undeadpixel', 'chempack', ['python_version', 'java_version', 'r_version', 'rdkit_release'])
+    image_builder = ChemPackDockerImageBuilder('undeadpixel', 'chempack', ['python_version', 'java_version', 'rdkit_release'])
     image_builder.build()
 
